01818408b71c9d6249ef4c088be11ac7f50e8c28
ankush1802 <ankush1802@outlook.com>
2022-05-23 14:18:39 +0530

    master

---
+++ Ajax_Crud_App/Models/JsonResponseViewModel.cs
@@ -0,0 +1,11 @@
+﻿namespace Ajax_Crud_App.Models
+{
+    [Serializable]
+    public class JsonResponseViewModel
+    {
+        public int ResponseCode { get; set; }
+
+        public string ResponseMessage { get; set; } = string.Empty;
+    }
+}
+
---
+++ Ajax_Crud_App/Models/StudentModel.cs
@@ -0,0 +1,9 @@
+﻿namespace Ajax_Crud_App.Models
+{
+    public class StudentModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+    }
+}
